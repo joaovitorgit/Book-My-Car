d56bf26ac954dec9bed4d943e35d555014f21eb1
Fulimirani <fulimirani@gmail.com>
2023-02-24 07:16:15 -0300

    24-02

--- test/unitTests/carService.test.js
+++ test/unitTests/carService.test.js
@@ -1,9 +1,16 @@
 const {describe, it} = require('mocha')
 const CarService = require('../../src/service/carService')
 const {join} = require('path')
-
-
+const assert = require('assert')
+const {expect} = require('chai')
 const carsDatabase = join(__dirname, "./../../database", "cars.json")
+
+const mocks = {
+    validCarCategory: require('./../mocks/valid-carCategory.json'),
+    validCar: require('./../mocks/valid-car.json'),
+    validCustomer: require('./../mocks/valid-customer.json'),
+}
+
 
 describe('CarService Suite tests', ()=>{
     let carService = {}
@@ -12,9 +19,20 @@ describe('CarService Suite tests', ()=>{
             cars: carsDatabase
         })
     })
-    it('Must return a avaiable car of a given car category', async ()=>{
-        const result = await carService.test("ecfd1ed1-8640-4625-8440-2edd51e8b07f")
-        console.log(result)
+    it("Should retrieve a random position from an array", async()=>{
+        const data = [ 0 , 1, 2, 3, 4]
+        const result = carService.getRandomPositionFromArray(data)
+
+        expect(result).to.be.lte(data.length).and.be.gte(0)
     })
-    // 8:54
+    // it('Must return a avaiable car of a given car category', async ()=>{
+    //     const car = mocks.validCar
+    //     const carCategory = Object.create(mocks.validCarCategory) 
+    //     carCategory.ids  = [car.id]
+    //     const result = await carService.getAvailableCar(carCategory)
+    //     const expected = car
+
+    //     expect(result).to.be.deep.equal(expected)
+    // })
+    
 })
